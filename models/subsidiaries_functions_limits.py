2b366eee7683736cc260e80e1e4ba073e0ed5219
postos-graciosa-dev <desenvolvimentopostosgraciosa@gmail.com>
2024-12-09 16:07:16 -0300

    feature: filter scale

---
+++ models/subsidiaries_functions_limits.py
@@ -0,0 +1,8 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class Subsidiarie(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    adress: str = Field(index=True)
+    phone: str = Field(index=True)
+    email: str = Field(index=True)

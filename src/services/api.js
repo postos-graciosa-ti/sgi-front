70e5e115a3012f4b37a1ac01f06ca679363fc14e
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-01-02 14:50:01 -0300

    minor fix: axios headers

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,9 @@ import axios from "axios"
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
-  // headers: {'X-Custom-Header': 'foobar'}
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 export default api

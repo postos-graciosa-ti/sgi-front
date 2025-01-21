98af7a2cade641d5fa080a3f22ee16e82457c5b2
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-01-21 17:20:41 -0300

    add departmente modal

--- src/pages/department/Department.jsx
+++ src/pages/department/Department.jsx
@@ -1,16 +1,25 @@
-import { useEffect, useState } from "react"
-import { Pencil, Plus, Question, Trash } from "react-bootstrap-icons"
-import Nav from "../../components/Nav"
-import api from "../../services/api"
+import { useEffect, useState } from "react";
+import { Pencil, Plus, Trash } from "react-bootstrap-icons";
+import Nav from "../../components/Nav";
+import api from "../../services/api";
+import AddDepartmentModal from "./AddDepartmentModal";
 
 const Department = () => {
-  const [departmentsList, setDepartmentsList] = useState()
+  const [departmentsList, setDepartmentsList] = useState([]);
+  const [addDepartmentModalOpen, setAddDepartmentModalOpen] = useState(false);
 
   useEffect(() => {
-    api
-      .get("/departments")
-      .then((response) => setDepartmentsList(response.data))
-  }, [])
+    const fetchDepartments = async () => {
+      try {
+        const response = await api.get("/departments");
+        setDepartmentsList(response.data);
+      } catch (error) {
+        console.error("Error fetching departments:", error);
+      }
+    };
+
+    fetchDepartments();
+  }, []);
 
   return (
     <>
@@ -23,19 +32,10 @@ const Department = () => {
 
         <div className="mt-3 mb-3">
           <button
-            id="help"
-            type="button"
-            className="btn btn-warning me-2"
-          // onClick={setTour}
-          >
-            <Question />
-          </button>
-
-          <button
             id="addTurn"
             type="button"
             className="btn btn-primary"
-          // onClick={handleOpenAddTurnModal}
+            onClick={() => setAddDepartmentModalOpen(true)}
           >
             <Plus />
           </button>
@@ -46,49 +46,47 @@ const Department = () => {
             <thead>
               <tr>
                 <th>Nome</th>
-
                 <th>Descrição</th>
-
                 <th></th>
               </tr>
             </thead>
-
             <tbody>
-              {
-                departmentsList && departmentsList.map((department) => (
-                  <tr>
-                    <td>{department.name}</td>
-
-                    <td>{department.description}</td>
-
-                    <td>
-                      <button
-                        id="editTurn"
-                        type="button"
-                        className="btn btn-warning mt-2 me-2"
-                      // onClick={() => handleOpenEditTurnModal(turn)}
-                      >
-                        <Pencil />
-                      </button>
-
-                      <button
-                        id="deleteTurn"
-                        type="button"
-                        className="btn btn-danger mt-2"
-                      // onClick={() => handleOpenDeleteTurnModal(turn)}
-                      >
-                        <Trash />
-                      </button>
-                    </td>
-                  </tr>
-                ))
-              }
+              {departmentsList && departmentsList.map((department) => (
+                <tr key={department.id}>
+                  <td>{department.name}</td>
+                  <td>{department.description}</td>
+                  <td>
+                    <button
+                      id="editTurn"
+                      type="button"
+                      className="btn btn-warning mt-2 me-2"
+                    // onClick={() => handleOpenEditTurnModal(turn)}
+                    >
+                      <Pencil />
+                    </button>
+                    <button
+                      id="deleteTurn"
+                      type="button"
+                      className="btn btn-danger mt-2"
+                    // onClick={() => handleOpenDeleteTurnModal(turn)}
+                    >
+                      <Trash />
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
       </div>
-    </>
-  )
-}
 
-export default Department
+      <AddDepartmentModal
+        addDepartmentModalOpen={addDepartmentModalOpen}
+        setAddDepartmentModalOpen={setAddDepartmentModalOpen}
+        setDepartmentsList={setDepartmentsList}
+      />
+    </>
+  );
+};
+
+export default Department;

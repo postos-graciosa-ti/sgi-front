7e9946df8ef59f749fd2996ff006b42220bdeecd
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-02-05 11:03:24 -0300

    fix

--- src/pages/scale/DeleteScaleModal.jsx
+++ src/pages/scale/DeleteScaleModal.jsx
@@ -22,6 +22,9 @@ const DeleteScaleModal = (props) => {
   const handleRemoveDayOff = () => {
     let updatedDaysOff = daysOff.filter((dayOff) => dayOff != moment(selectedDate).format("DD-MM-YYYY"))
 
+    console.log(updatedDaysOff)
+    debugger
+
     if (daysOff.length == 1) {
       setDaysOff(updatedDaysOff)
 
@@ -66,9 +69,15 @@ const DeleteScaleModal = (props) => {
       "ilegal_dates": `[${updatedDaysOff.map(dayOff => `'${dayOff}'`).join(',')}]`
     }
 
+    console.log(formData)
+    debugger
+
     api
-      .post(`/scales`, formData)
+      .post("/delete-scale", formData)
       .then((response) => {
+        console.log(response)
+        debugger
+
         setDeleteScaleModalOpen(false)
 
         api

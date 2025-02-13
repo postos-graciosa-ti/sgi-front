022a0e7e04b950ab10fa782b2d4712d90f34c6c4
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-02-13 13:44:02 -0300

    fix

---
+++ src/pages/logsPages/SubsidiariesLogs.jsx
@@ -0,0 +1,13 @@
+import Nav from "../../components/Nav"
+
+const SubsidiariesLogs = () => {
+  return (
+    <>
+      <Nav />
+
+      <div className="container"></div>
+    </>
+  )
+}
+
+export default SubsidiariesLogs

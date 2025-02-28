ff091b6b700bcc82e48fa9d366a9534d2f5610a7
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-02-28 09:34:53 -0300

    feature: add sundays count

--- src/pages/scale/printContent.jsx
+++ src/pages/scale/printContent.jsx
@@ -15,6 +15,12 @@ const printContent = (scalesList, onDuty, startDate, endDate) => {
     return days[weekday] || ""
   }
 
+  const countSundays = (scale) => {
+    return scale.days_off?.filter((dateItem) => {
+      return moment(dateItem, "YYYY-MM-DD").format("dddd") === "Sunday";
+    }).length || 0
+  }
+
   return (
     <div className="container">
       <h3>
@@ -30,73 +36,62 @@ const printContent = (scalesList, onDuty, startDate, endDate) => {
           <thead>
             <tr>
               <th>Colaborador</th>
+              <th>Domingos de Folga</th>
               <th>Dias de Trabalho</th>
               <th>Folga</th>
             </tr>
           </thead>
 
           <tbody>
-            {
-              scalesList?.map((scale) => (
-                <tr key={scale.worker.id}>
-                  <td>{scale.worker.name}</td>
+            {scalesList?.map((scale) => (
+              <tr key={scale.worker.id}>
+                <td>{scale.worker.name}</td>
 
-                  <td>
-                    <div className="badge-container">
-                      {
-                        scale.days_on?.map((dateItem, index) => {
-                          const weekdayInEnglish = moment(dateItem, "YYYY-MM-DD").format("dddd")
+                <td>{countSundays(scale)}</td>
 
-                          return (
-                            <div key={index} className="badge text-bg-primary">
-                              {`${moment(dateItem).format("DD-MM-YYYY")} (${translateWeekday(weekdayInEnglish)})`}
-                            </div>
-                          )
-                        })
-                      }
-                    </div>
-                  </td>
+                <td>
+                  <div className="badge-container">
+                    {scale.days_on?.map((dateItem, index) => {
+                      const weekdayInEnglish = moment(dateItem, "YYYY-MM-DD").format("dddd");
+                      return (
+                        <div key={index} className="badge text-bg-primary">
+                          {`${moment(dateItem).format("DD-MM-YYYY")} (${translateWeekday(weekdayInEnglish)})`}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </td>
 
-                  <td>
-                    <div className="badge-container">
-                      {
-                        scale.days_off?.map((dateItem, index) => {
-                          const weekdayInEnglish = moment(dateItem, "YYYY-MM-DD").format("dddd");
-
-                          return (
-                            <div key={index} className="badge text-bg-success">
-                              {`${moment(dateItem).format("DD-MM-YYYY")} (${translateWeekday(weekdayInEnglish)})`}
-                            </div>
-                          )
-                        })
-                      }
-                    </div>
-                  </td>
-                </tr>
-              ))
-            }
+                <td>
+                  <div className="badge-container">
+                    {scale.days_off?.map((dateItem, index) => {
+                      const weekdayInEnglish = moment(dateItem, "YYYY-MM-DD").format("dddd");
+                      return (
+                        <div key={index} className="badge text-bg-success">
+                          {`${moment(dateItem).format("DD-MM-YYYY")} (${translateWeekday(weekdayInEnglish)})`}
+                        </div>
+                      )
+                    })}
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
 
       <div>
-        <h3>Assinaturas:</h3>
-
-        {
-          scalesList?.map((scale) => (
-            <div key={scale.worker.id}>
-              {scale.worker.name} __________________________________________________
-            </div>
-          ))
-        }
+        <h3>Plantão</h3>
+        <div>{onDuty}</div>
       </div>
 
       <div>
-        <h3>Plantão</h3>
-
-        <div>
-          {onDuty}
-        </div>
+        <h3>Assinaturas:</h3>
+        {scalesList?.map((scale) => (
+          <div key={scale.worker.id}>
+            {scale.worker.name} __________________________________________________
+          </div>
+        ))}
       </div>
     </div>
   )

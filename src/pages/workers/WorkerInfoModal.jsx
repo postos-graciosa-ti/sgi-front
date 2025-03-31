7f312eaafd8750eac1c2877e73d2902736791e2c
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-03-31 09:57:12 -0300

    add

--- src/pages/workers/WorkerInfoModal.jsx
+++ src/pages/workers/WorkerInfoModal.jsx
@@ -319,7 +319,7 @@ const WorkerInfoModal = (props) => {
             className="form-control"
             aria-label="With textarea"
             rows={1}
-            value={selectedWorker?.birthcity || "Não consta registro"}
+            value={selectedWorker?.birthcity?.name || "Não consta registro"}
             disabled
           />
         </div>
@@ -368,6 +368,438 @@ const WorkerInfoModal = (props) => {
             aria-label="With textarea"
             rows={1}
             value={selectedWorker?.cpf || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">RG</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.rg || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Órgão emissor de RG</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.rg_issuing_agency || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Estado de RG</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.rg_state || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Data de expedição de RG</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={moment(selectedWorker?.rg_expedition_date).format("DD-MM-YYYY") || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Certificado de reservista</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.military_cert_number || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">PIS</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.pis || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Data de cadastro de PIS</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.pis_register_date || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Título de eleitor</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.votant_title || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Zona eleitoral</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.votant_zone || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Sessão eleitoral</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.votant_session || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">CTPS</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.ctps || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Série de CTPS</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.ctps_serie || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Estado de CTPS</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.ctps_state?.name || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Data de emissão de CTPS</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={moment(selectedWorker?.ctps_emission_date).format("DD-MM-YYYY") || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">CNH</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.cnh || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Categoria de CNH</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.cnh_category || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Data de emissão de CNH</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={moment(selectedWorker?.cnh_emition_date).format("DD-MM-YYYY") || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Validade de CNH</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={moment(selectedWorker?.cnh_valid_date).format("DD-MM-YYYY") || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Primeiro emprego?</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.first_job && "Sim" || "Não"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Já foi funcionário da empresa?</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.was_employee && "Sim" || "Não"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Deu contribuição sindical nesse ano?</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.union_contribute_current_year && "Sim" || "Não"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Recebendo seguro desemprego?</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.receiving_unemployment_insurance && "Sim" || "Não"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Experiência prévia na função?</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.previous_experience && "Sim" || "Não"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Salário mensal</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.month_wage || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Salário por hora</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.hour_wage || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Salário proporcional à jornada</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.journey_wage || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Vale transporte</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.transport_voucher || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Quantidade de vale transporte</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.transport_voucher_quantity || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Jornada de trabalho diária</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.diary_workjourney || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Jornada de trabalho semanal</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.week_workjourney || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Jornada de trabalho mensal</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.month_workjourney || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Tempo de experiência</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.experience_time || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Horas noturnas</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.nocturne_hours || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Periculosidade</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.dangerousness || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Insalubridade</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.unhealthy || "Não consta registro"}
+            disabled
+          />
+        </div>
+
+        <div className="input-group mb-3">
+          <span className="input-group-text fw-bold">Método de pagamento</span>
+
+          <textarea
+            className="form-control"
+            aria-label="With textarea"
+            rows={1}
+            value={selectedWorker?.wage_payment_method || "Não consta registro"}
             disabled
           />
         </div>

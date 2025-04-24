3a72dcabdef796b46d2416175126609e64337a2f
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-04-24 16:47:41 -0300

    add docs title

--- src/pages/workers/WorkerDocsModal.jsx
+++ src/pages/workers/WorkerDocsModal.jsx
@@ -4,11 +4,14 @@ import { Plus, Trash } from 'react-bootstrap-icons'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import api from '../../services/api'
+import Select from '../../components/form/Select'
 
 const WorkerDocsModal = (props) => {
   const { workerDocsModalOpen, setWorkerDocsModalOpen, selectedWorker, setSelectedWorker } = props
 
   const [docsList, setDocsList] = useState()
+
+  const [docTitle, setDocTitle] = useState()
 
   const [doc, setDoc] = useState()
 
@@ -32,7 +35,8 @@ const WorkerDocsModal = (props) => {
     axios
       .post(`${import.meta.env.VITE_API_URL}/upload-pdf/${selectedWorker?.worker_id}`,
         {
-          "file": doc
+          "file": doc,
+          "doc_title": docTitle.label
         },
         {
           headers: {
@@ -73,6 +77,28 @@ const WorkerDocsModal = (props) => {
       </Modal.Header>
 
       <Modal.Body>
+        <div>
+          <Select
+            label={"Título do documento"}
+            placeholder={""}
+            options={[
+              { value: 1, label: "CTPS" },
+              { value: 2, label: "Exame médico admissional" },
+              { value: 3, label: "Identidade" },
+              { value: 4, label: "CPF" },
+              { value: 5, label: "Titulo eleitoral" },
+              { value: 7, label: "Comprovante de residência" },
+              { value: 8, label: "CNH" },
+              { value: 9, label: "Certidão de casamento" },
+              { value: 10, label: "Certificado de reservista" },
+              { value: 11, label: "Certidão de nascimento (filhos menores de 14)" },
+              { value: 12, label: "Carteira de vacinação (filhos menores de 5)" },
+              { value: 13, label: "Comprovante de frequência escolar (filhos entre 7 e 14)" },
+            ]}
+            setSelectedValue={setDocTitle}
+          />
+        </div>
+
         <div className="row">
           <div className="col-10">
             <input type="file" className="form-control" onChange={(e) => setDoc(e.target.files[0])} />
@@ -90,7 +116,7 @@ const WorkerDocsModal = (props) => {
             <div key={pdf.doc_id} className="mt-5 mb-5">
               <div className="row mb-2">
                 <div className="col-10">
-                  <h3>Documento #{pdf.doc_id}</h3>
+                  <h4> {`${pdf.doc_title}` || `Documento #${pdf.doc_id}`}</h4>
                 </div>
 
                 <div className="col-2">

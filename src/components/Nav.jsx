9c602627a252a25649ec98f38a3e00f4852ed8b8
postos-graciosa-ti <postosgraciosati@gmail.com>
2025-06-16 17:18:16 -0300

    fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -120,6 +120,8 @@ const Nav = () => {
 
   const [changePasswordModalOpen, setChangePasswordModalOpen] = useState(false)
 
+  const [subsidiarieData, setSubsidiarieData] = useState()
+
   useEffect(() => {
     api
       .get(`/subsidiaries/${selectedSubsidiarie?.value}/workers/experience-time-no-first-review`)
@@ -128,13 +130,17 @@ const Nav = () => {
     api
       .get(`/subsidiaries/${selectedSubsidiarie?.value}/workers/experience-time-no-second-review`)
       .then((response) => setWorkersSecondReview(response?.data))
+
+    api
+      .get(`/subsidiaries/${selectedSubsidiarie?.value}`)
+      .then((response) => setSubsidiarieData(response.data))
   }, [])
 
   return (
     <>
       <nav id="navbar" className="navbar navbar-expand-lg bg-body-tertiary mb-5">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">SGI</a>
+          <a className="navbar-brand" href="#">{selectedSubsidiarie?.label} {subsidiarieData?.cnpj}</a>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -143,16 +149,8 @@ const Nav = () => {
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <span className="nav-link">{selectedSubsidiarie?.label}</span>
-              </li>
-
-              <li className="nav-item">
                 <Link className="nav-link" to="/home">Início</Link>
               </li>
-
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/update-notifications">Histórico de atualizações</Link>
-              </li> */}
 
               <li className="nav-item">
                 <Link className="nav-link" to="/positions-table">
@@ -165,26 +163,6 @@ const Nav = () => {
                   Processos seletivos
                 </Link>
               </li>
-
-              {/* <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Contratações
-                </a>
-
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="/positions-table">
-                      Quadro de vagas
-                    </Link>
-                  </li>
-
-                  <li>
-                    <Link className="dropdown-item" to="/applicants">
-                      Processos seletivos
-                    </Link>
-                  </li>
-                </ul>
-              </li> */}
 
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -250,17 +228,11 @@ const Nav = () => {
                     <ul className="dropdown-menu">
                       <li><Link className="dropdown-item" to="/system-log">Todos</Link></li>
 
-                      {/* <li><Link className="dropdown-item" to="/workers-logs">Colaboradores</Link></li> */}
-
                       <li><Link className="dropdown-item" to="/turns-logs">Turnos</Link></li>
 
                       <li><Link className="dropdown-item" to="/cost-center-logs">Centros de custos</Link></li>
 
                       <li><Link className="dropdown-item" to="/department-logs">Setores</Link></li>
-
-                      {/* <li><Link className="dropdown-item" to="/subsidiaries-logs">Filiais</Link></li> */}
-
-                      {/* <li><Link className="dropdown-item" to="/users-logs">Usuários</Link></li> */}
 
                       <li><Link className="dropdown-item" to="/functions-logs">Funções</Link></li>
                     </ul>
@@ -308,58 +280,6 @@ const Nav = () => {
           </div>
         </div>
       </nav>
-
-      {/* {
-        workersFirstReview?.workers?.length > 0 && (
-          <div className="container">
-            {
-              workersFirstReview?.workers?.length > 0 && (
-                <div><h5>Funcionários que vão expirar o tempo de experiência (1° período) entre {moment(workersFirstReview.start_of_week).format("DD/MM/YYYY")} e {moment(workersFirstReview.end_of_week).format("DD/MM/YYYY")}</h5></div>
-              )
-            }
-
-            <div className="d-inline-flex">
-              {
-                workersFirstReview && (
-                  workersFirstReview?.workers?.map((worker) => (
-                    <div>
-                      <div className="alert alert-danger me-1">
-                        {worker.name} ({moment(worker.first_review_date).format("DD-MM-YYYY")})
-                      </div>
-                    </div>
-                  ))
-                )
-              }
-            </div>
-          </div>
-        )
-      }
-
-      {
-        workersSecondReview?.workers?.length > 0 && (
-          <div className="container">
-            {
-              workersSecondReview?.workers?.length > 0 && (
-                <div><h5>Funcionários que vão expirar o tempo de experiência (2° período) entre {moment(workersSecondReview.start_of_week).format("DD/MM/YYYY")} e {moment(workersSecondReview.end_of_week).format("DD/MM/YYYY")}</h5></div>
-              )
-            }
-
-            <div className="d-inline-flex">
-              {
-                workersSecondReview && (
-                  workersSecondReview?.workers?.map((worker) => (
-                    <div>
-                      <div className="alert alert-danger me-1">
-                        {worker.name} ({moment(worker.second_review_date).format("DD-MM-YYYY")})
-                      </div>
-                    </div>
-                  ))
-                )
-              }
-            </div>
-          </div>
-        )
-      } */}
 
       <ChangePasswordModal
         changePasswordModalOpen={changePasswordModalOpen}
